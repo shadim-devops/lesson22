7ec88eb755d590f3f9644c2a9ad6df1aef83adb9
shadim-devops <shafranski777@gmail.com>
2026-03-05 21:46:39 +0100

    fix multiplication table formatting

--- zadanie2/tablica.py
+++ zadanie2/tablica.py
@@ -1,6 +1,12 @@
 print("Tablica mnożenia")
 
+print("   ", end="")
 for i in range(1, 11):
+    print(f"{i:4}", end="")
+print()
+
+for i in range(1, 11):
+    print(f"{i:3}", end="")
     for j in range(1, 11):
-        print(i * j, end=" ")
+        print(f"{i*j:4}", end="")
     print()
